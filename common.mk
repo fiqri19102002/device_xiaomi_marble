4129957fef96589b6d8929d93273e5b50d3c58f6
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-01-23 10:04:10 +0700

    fixup! sm8450-common: Enable ELF checks for libwfdmmsrc_proprietary
    libwfdaac_vendor has been converted to blueprint, so there is
    no need to build libwfdaac_vendor manually.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- common.mk
+++ common.mk
@@ -468,9 +468,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
 # WiFi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor:64
-
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
